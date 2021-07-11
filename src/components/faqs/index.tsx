f8ccd51cda92f2f17d5bfcf938e616703d0bb917
david <davidmirandagrinstein@gmail.com>
2021-07-11 18:21:31 -0400

    faqs

--- src/components/faqs/index.tsx
+++ src/components/faqs/index.tsx
@@ -1,52 +1,81 @@
-import React from 'react';
-// import { Jumbotron, Container, Row, Col, Accordion, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Accordion, AccordionTitleProps, Container, Grid, Icon } from 'semantic-ui-react';
+import styles from './index.module.scss';
 
 const Faqs = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, data: AccordionTitleProps) => {
+        setActiveIndex(Number(data.index));
+    };
+
     return (
-        <>
-            {/* <Container>
-                <Row>
-                    <Col>
-                        <h2 style={{ color: 'grey' }}>The most frequent asked questions</h2>
-                        <h1 style={{ color: 'black' }}>FAQS</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Accordion defaultActiveKey="">
-                            <Card>
-                                <Accordion.Toggle as={Card.Header} eventKey="0">
-                                    What are the other charges I will have to pay?
-                                </Accordion.Toggle>
-                                <Accordion.Collapse eventKey="0">
-                                    <Card.Body>
-                                        Duis bibendum diam non erat facilaisis tincidunt. Fusce leo neque, lacinia at
-                                        tempor vitae, porta at arcu. Vestibulum varius non dui at pulvinar. Ut egestas
-                                        orci in quam sollicitudin aliquet.
-                                    </Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
+        <Container fluid textAlign="center" className={styles.container}>
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column width="16">
+                        <h3 style={{ color: 'grey', fontWeight: 'lighter', marginBottom: 0 }}>
+                            The most frequent asked questions
+                        </h3>
+                        <h1 style={{ color: 'black', marginTop: 0, fontWeight: 'bolder' }}>FAQS</h1>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width="8">
+                        <Accordion fluid styled>
+                            <Accordion.Title active={activeIndex === 1} index={1} onClick={handleClick}>
+                                <Icon name="dropdown" />
+                                What is a dog?
+                            </Accordion.Title>
+                            <Accordion.Content active={activeIndex === 1}>
+                                <p>
+                                    A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it
+                                    can be found as a welcome guest in many households across the world.
+                                </p>
+                            </Accordion.Content>
+
+                            <Accordion.Title active={activeIndex === 2} index={2} onClick={handleClick}>
+                                <Icon name="dropdown" />
+                                What kinds of dogs are there?
+                            </Accordion.Title>
+                            <Accordion.Content active={activeIndex === 2}>
+                                <p>
+                                    There are many breeds of dogs. Each breed varies in size and temperament. Owners
+                                    often select a breed of dog that they find to be compatible with their own lifestyle
+                                    and desires from a companion.
+                                </p>
+                            </Accordion.Content>
                         </Accordion>
-                    </Col>
-                    <Col>
-                        <Accordion defaultActiveKey="">
-                            <Card>
-                                <Accordion.Toggle as={Card.Header} eventKey="0">
-                                    Where can I contact your agent?
-                                </Accordion.Toggle>
-                                <Accordion.Collapse eventKey="0">
-                                    <Card.Body>
-                                        Duis bibendum diam non erat facilaisis tincidunt. Fusce leo neque, lacinia at
-                                        tempor vitae, porta at arcu. Vestibulum varius non dui at pulvinar. Ut egestas
-                                        orci in quam sollicitudin aliquet.
-                                    </Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
+                    </Grid.Column>
+                    <Grid.Column width="8">
+                        <Accordion fluid styled>
+                            <Accordion.Title active={activeIndex === 3} index={3} onClick={handleClick}>
+                                <Icon name="dropdown" />
+                                What is a dog?
+                            </Accordion.Title>
+                            <Accordion.Content active={activeIndex === 3}>
+                                <p>
+                                    A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it
+                                    can be found as a welcome guest in many households across the world.
+                                </p>
+                            </Accordion.Content>
+
+                            <Accordion.Title active={activeIndex === 4} index={4} onClick={handleClick}>
+                                <Icon name="dropdown" />
+                                What kinds of dogs are there?
+                            </Accordion.Title>
+                            <Accordion.Content active={activeIndex === 4}>
+                                <p>
+                                    There are many breeds of dogs. Each breed varies in size and temperament. Owners
+                                    often select a breed of dog that they find to be compatible with their own lifestyle
+                                    and desires from a companion.
+                                </p>
+                            </Accordion.Content>
                         </Accordion>
-                    </Col>
-                </Row>
-            </Container> */}
-        </>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Container>
     );
 };
 
