c1963522df515f2287c47ab7c64680e6c365f3e8
david <davidmirandagrinstein@gmail.com>
2021-07-12 12:10:16 -0400

    refactor

---
+++ src/components/contact_us/form.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button, Grid, Input, TextArea } from 'semantic-ui-react';
+
+const ContactForm = () => {
+    return (
+        <Grid>
+            <Grid.Row>
+                <Grid.Column width="8">
+                    <Input fluid placeholder="Name" />
+                </Grid.Column>
+                <Grid.Column width="8">
+                    <Input fluid placeholder="Email" />
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column width="16">
+                    <Input fluid placeholder="Subject" />
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column width="16">
+                    <TextArea style={{ width: '100%' }}></TextArea>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column width="16">
+                    <Button content="SEND MESSAGE" fluid />
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    );
+};
+
+export default ContactForm;
--- src/components/contact_us/index.tsx
+++ src/components/contact_us/index.tsx
@@ -1,36 +1,7 @@
-import Container from 'components/container';
 import React from 'react';
-import { Button, Grid, Icon, Input, TextArea } from 'semantic-ui-react';
-
-const ContactForm = () => {
-    return (
-        <Grid>
-            <Grid.Row>
-                <Grid.Column width="8">
-                    <Input fluid placeholder="Name" />
-                </Grid.Column>
-                <Grid.Column width="8">
-                    <Input fluid placeholder="Email" />
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column width="16">
-                    <Input fluid placeholder="Subject" />
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column width="16">
-                    <TextArea style={{ width: '100%' }}></TextArea>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column width="16">
-                    <Button content="SEND MESSAGE" fluid />
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-    );
-};
+import Container from 'components/container';
+import { Grid, Icon } from 'semantic-ui-react';
+import ContactForm from './form';
 
 const ContactUs = () => {
     return (
